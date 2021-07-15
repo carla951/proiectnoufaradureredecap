611528ff9f46d40e5f772c35bdaa1e9a01cb5bc9
carla951 <carla.popescu95@gmail.com>
2021-07-15 20:34:52 +0300

    hehe

--- src/main/java/tests/LoginTest.java
+++ src/main/java/tests/LoginTest.java
@@ -2,15 +2,9 @@ package tests;
 
 import static org.testng.Assert.assertEquals;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
+
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.Properties;
-import java.util.Scanner;
+
 
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
@@ -32,7 +26,7 @@ public class LoginTest extends BaseTest{
 		loginPage.setPassword(password);
 		
 		LoggedUser loggedUser = loginPage.clickSubmitButton();
-		assertEquals(loggedUser.readUserName(), "Test User");
+		assertEquals(loggedUser.readUserName(), "Test User6");
 		
 	
 	}
